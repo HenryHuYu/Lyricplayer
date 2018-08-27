41508be0296a0090f2f579d278a7cd0b905ea15f
Yu Hu <443303575@qq.com>
2018-08-27 17:24:37 +0800

    Add files via upload
    将原来的类拆分，分出一个player类，player类的初始化要传入一个Window类的实例，这样的话，player类里面就可以通过self.parent.XXX来更改UI界面，比如点击暂停，将parent的播放按钮就可以self.parent.playbutton.hide()隐藏起来。
    也就是说，player这个类负责所有播放相关的事物，通过player.parent来修改UI界面中与播放相关的部分。
    所有的类若涉及和界面有交互，都可以传入一个Window类实例，从而访问到与之相关的按钮控件等。

--- MainWindow.py
+++ MainWindow.py
@@ -18,155 +18,21 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setupUi(self)
-        self.lrc_reader = TxtReader(url='/Users/yuhu/Desktop/日语歌曲翻译/歌词翻译txt版/Lemon.txt')
         self.current_block = 0
 
-        self.player = QMediaPlayer()
-        self.song_duration = eyed3.load(WindowCtrl.path).info.time_secs
+        self.player = Player(self)
 
-        self.player.bufferStatusChanged.connect(self.bufferingProgress)
-        self.player.durationChanged.connect(self.duration_changed)
-        self.playbutton.clicked.connect(self.play_or_pause)
-        self.pausebutton.clicked.connect(self.play_or_pause)
-        self.openfilebutton.clicked.connect(self.openfiles)
-        self.pausebutton.hide()
-
-        self.vmutebutton.clicked.connect(self.set_volume)
-        self.volumebutton.clicked.connect(self.mute_volume)
-        self.vmutebutton.hide()
-        self.volumeslider.setRange(0, 100)
-        self.volumeslider.setValue(100)
-        self.volumeslider.sliderMoved.connect(self.set_volume)
-
-        self.progress_slider.sliderMoved.connect(self.position_set)
-        self.progress_slider.setRange(0, self.song_duration)
 
         self.lrc_textBrowser.setFont(QFont('宋体', 25))
-
-        h = '<html><body><p style="font-family:verdana;font-size:80%;color:green">\
-            This is a paragraph with some text in it. This is a paragraph with some text in it. <br>\
-            This is a paragraph with some text in it. This is a paragraph with some text in it.\
-            </p>\
-            </body>\
-            </html>'
-        self.ori_webEngineView.setHtml(h)
 
         with open('QSS/playerbutton.qss', 'r', encoding='utf-8') as f:
             self.setStyleSheet(f.read())
 
-        self.prettifier = Prettifier()
-
-        self.player.positionChanged.connect(self.position_changed)
-        self.player.stateChanged.connect(self.state_changed)
-
-        self.url = QUrl.fromLocalFile(WindowCtrl.path)
-        self.content = QMediaContent(self.url)
-        self.player.setMedia(self.content)
-
-    def play_or_pause(self):
-
-        if self.player.state() in (QMediaPlayer.StoppedState, QMediaPlayer.PausedState):
-            self.player.play()
-            self.pausebutton.show()
 
 
-        elif self.player.state() == QMediaPlayer.PlayingState:
-            self.player.pause()
-            self.pausebutton.hide()
 
-    def state_changed(self, state):
-        if state == QMediaPlayer.StoppedState:
-            self.current_block = 0
 
-    def duration_changed(self, duration):
-        duration /= 1000
 
-    def setup_slider(self):
-        pass
-
-    def position_set(self, position):
-        value = position * 1000
-        self.player.setPosition(value)
-
-        for i in range(0, self.lrc_reader.block_amount):
-            if value < self.lrc_reader.timeaxis_lst[i]:
-                self.current_block = max(0, i - 1)
-                break
-
-    def mute_volume(self):
-        self.player.setVolume(0)
-        self.vmutebutton.show()
-
-    def set_volume(self):
-        self.player.setVolume(self.volumeslider.value())
-        self.vmutebutton.hide()
-
-    def bufferingProgress(self):
-        pass
-
-    def position_changed(self, position):
-        self.progress_slider.setValue(position / 1000)
-
-        def min_sec(x):
-            return '{}'.format(int(x / 60)).zfill(2) + ':' + '{}'.format(int(x % 60)).zfill(2)
-
-        self.songtimelabel.setText('{}/{}'.format(min_sec(position / 1000), min_sec(self.song_duration)))
-
-        if self.current_block >= self.lrc_reader.block_amount:
-            return
-        if position > self.lrc_reader.timeaxis_lst[self.current_block]:
-            self.lrc_textBrowser.setPlainText('')
-
-            analyzer_no_parenthesis = self.prettifier.delete_parenthesis(
-                self.lrc_reader.analyzer_lst[self.current_block])
-            self.lrc_textBrowser.append(analyzer_no_parenthesis)
-
-            # ori_pron = self.get_block_ori_with_pronounce(self.current_block)
-            # ori_chg_font = self.prettifier.change_font(ori_pron)
-
-            # trans = self.prettifier.change_font(self.lrc_reader.trans_lst[self.current_block])
-            html = self.lrc_html_for_roller(3)
-
-            self.ori_webEngineView.setHtml(html)
-
-            self.current_block += 1
-
-    ##suitable for odd
-    def lrc_html_for_roller(self, line_to_show):
-        html = ''
-        range_block_to_show = []
-        if self.current_block == 0:
-            range_block_to_show = range(0, line_to_show)
-        elif self.current_block == self.lrc_reader.block_total - 1:
-            range_block_to_show = range(self.current_block - line_to_show + 1, self.current_block + 1)
-        else:
-            range_block_to_show = range(self.current_block - int((line_to_show - 1) / 2), self.current_block +
-                                        int((line_to_show - 1) / 2) + 1)
-
-        for i in range_block_to_show:
-            if i == self.current_block:
-                html += self.prettifier.change_font(self.get_block_ori_with_pronounce(i), color='orange') \
-                        + '<br>' + \
-                        self.prettifier.change_font(self.lrc_reader.translate_lst[i], color='orange') + '<br>' + '<br>'
-            else:
-                html += self.prettifier.change_font(self.get_block_ori_with_pronounce(i)) + \
-                        '<br>' + self.lrc_reader.translate_lst[i] + '<br>' + '<br>'
-            self.prettifier.change_font(html)
-        return html
-
-    def get_block_ori_with_pronounce(self, block):
-        ori_sentence = self.lrc_reader.original_lst[block].split('.')[1]
-        analyze_sentence = self.lrc_reader.analyzer_lst[block]
-        pronounce = self.lrc_reader.find_kanji_hiragana(analyze_sentence)
-
-        pronounced_ready = ori_sentence
-        for k, v in pronounce.items():
-            pronounced_ready = pronounced_ready.replace(k, self.prettifier.pronounce_annotate(k, v))
-        return pronounced_ready
-
-    def openfiles(self):
-        song = QFileDialog.getOpenFileName(parent=self, caption='Select Song', filter='Sound Files(*.mp3)')
-        print(song)
 
 
 class Prettifier:
@@ -206,15 +72,9 @@ class Prettifier:
             return '<font face= \"宋体\" font size=\"4\" color=\"black\">{}</font>'.format(st)
 
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    ui = Window()
-    ui.show()
-    sys.exit(app.exec())
-
-
-
 # 真正的音乐播放器
+# lyric 感觉应该写入这个类， 也就是包含一个lyric 的变量
+
 class Player(QMediaPlayer):
     def __init__(self, parent: Window = None):   # 标注类型辅助，不知道标注Window好，还是Qframe
         super(Player, self).__init__()
@@ -222,7 +82,103 @@ class Player(QMediaPlayer):
         self.parent = parent
 
         self.playWidget = self.parent.bottomframe  #control widget
-        self.setPlaylist(QMediaPlaylist())
+        self.song_duration = eyed3.load(Window.path).info.time_secs
 
+        self.lrc = TxtReader(url='/Users/yuhu/Desktop/日语歌曲翻译/歌词翻译txt版/Lemon.txt')
+        self.currentLrc = 0
+
+        self.url = QUrl.fromLocalFile(Window.path)
+        self.content = QMediaContent(self.url)
+        self.setMedia(self.content)
+
+        self.setconnects()
+
+
+    def setconnects(self):
+        self.durationChanged.connect(self.duration_changed)
+        self.bufferStatusChanged.connect(self.buffering_progress)
+        self.positionChanged.connect(self.position_changed)
+
+        self.parent.playbutton.clicked.connect(self.play_or_pause)
+        self.parent.pausebutton.clicked.connect(self.play_or_pause)
+
+        self.parent.pausebutton.hide()
+
+        self.parent.vmutebutton.clicked.connect(self.set_volume)
+        self.parent.volumebutton.clicked.connect(self.mute_volume)
+        self.parent.vmutebutton.hide()
+        self.parent.volumeslider.setRange(0, 100)
+        self.parent.volumeslider.setValue(100)
+        self.parent.volumeslider.sliderMoved.connect(self.set_volume)
+
+        self.parent.progress_slider.sliderMoved.connect(self.position_set)
+        self.parent.progress_slider.setRange(0, self.song_duration)
+
+
+    def duration_changed(self, duration):
+        print(duration)
+        duration /= 1000
+
+    def buffering_progress(self, buffer):
+        print(buffer)
+
+    def play_or_pause(self):
+        if self.state() in (QMediaPlayer.StoppedState, QMediaPlayer.PausedState):
+            self.play()
+            self.parent.pausebutton.show()
+
+        elif self.state() == QMediaPlayer.PlayingState:
+            self.pause()
+            self.parent.pausebutton.hide()
+
+    def state_changed(self, state):
+        if state == QMediaPlayer.StoppedState:
+            self.currentLrc = 0
+
+    def mute_volume(self):
+        self.setVolume(0)
+        self.parent.volumebutton.hide()
+        self.parent.vmutebutton.show()
+
+    def set_volume(self):
+        self.setVolume(self.parent.volumeslider.value())
+        print(self.parent.volumeslider.value())
+        self.parent.vmutebutton.hide()
+        self.parent.volumebutton.show()
+
+    def position_set(self, position):
+        value = position * 1000
+        self.setPosition(value)
+
+        for i in range(0, self.lrc.block_amount):
+            if value < self.lrc.timeaxis_lst[i]:
+                self.currentLrc = max(0, i - 1)
+                break
+
+    def position_changed(self, position):
+        self.parent.progress_slider.setValue(position / 1000)
+
+        def min_sec(x):
+            return '{}'.format(int(x / 60)).zfill(2) + ':' + '{}'.format(int(x % 60)).zfill(2)
+
+        self.parent.songtimelabel.setText('{}/{}'.format(min_sec(position / 1000), min_sec(self.song_duration)))
+
+        if self.currentLrc >= self.lrc.block_amount:
+            return
+        if position > self.lrc.timeaxis_lst[self.currentLrc]:
+            self.parent.lrc_textBrowser.setPlainText('')
+
+            analyzer_no_parenthesis = self.prettifier.delete_parenthesis(
+                self.lrc.analyzer_lst[self.currentLrc])
+            self.parent.lrc_textBrowser.append(analyzer_no_parenthesis)
+
+            self.currentLrc += 1
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ui = Window()
+    ui.show()
+    sys.exit(app.exec())
 
 
