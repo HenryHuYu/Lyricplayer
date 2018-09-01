1aebd4697604df8a4cba0f13b3f68d80a67eeaba
Yu Hu <443303575@qq.com>
2018-09-01 12:37:39 +0800

    Add files via upload
    修改了playlist中的各种bug，增加wannaplay的flag，以及statuschanged的控制，目前仍然有一个底部拖拽进度会有layout变动的bug。其余应该没有bug了

--- MainWindow.py
+++ MainWindow.py
@@ -9,8 +9,16 @@ import eyed3
 from TxtReader import TxtReader
 import re
 import random
+import threading
+import time
 from PyQt5.QtWidgets import QFontDialog
-
+'''
+问题汇总：
+1。 durationchanged
+2。 为何用Endofmedia无法自动切换到下一个歌曲，找到原因，是因为status会变为loaded media导致进入stopped状态。
+    目前用一个wanna play的flag进行解决
+3. 如果一个槽函数正在执行，一个新的信号发射了，会怎么样
+'''
 
 class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     # path = '/Users/yuhu/Music/网易云音乐/東山奈央/夏祭り.mp3'
@@ -19,7 +27,6 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setupUi(self)
-        self.current_block = 0
 
         self.player = Player(self)
 
@@ -72,7 +79,8 @@ class Prettifier:
 
 
 # 自定义继承自Qmediaplaylist的类
-# TODO 关于最开始初始状态的情况
+# TODO 关于最开始初始状态的情况，无歌单的初始化
+
 class PlayList(QMediaPlaylist):
     def __init__(self, parent: Window = None):
         super(PlayList, self).__init__()
@@ -82,39 +90,60 @@ class PlayList(QMediaPlaylist):
         self.playWidget = self.parent.bottomframe
 
         # list control
+        self.parent.playbackbutton_2.show()
+        self.parent.playbackbutton_3.hide()
+        self.parent.playbackbutton_1.hide()
+        self.setPlaybackMode(QMediaPlaylist.Sequential)
+
+
         self.indexlist = []
         self.curr_index = 0
+        self.str_urllist = []  # used for urls store
 
         # connects
         self.setconnects()
 
+    # text: 1 : ran, 2 : seq, 3 : loop
     def setconnects(self):
         self.mediaInserted.connect(self.inserted)
-        self.parent.playbackbutton_ran.clicked.connect(self.set_ranplayback)
-        self.parent.playbackbutton_seq.clicked.connect(self.set_seqplayback)
-        self.parent.playbackbutton_singleloop.clicked.connect(self.set_singleloopplayback)
-
-
+        self.parent.playbackbutton_2.clicked.connect(self.set_ranplayback)
+        self.parent.playbackbutton_3.clicked.connect(self.set_seqplayback)
+        self.parent.playbackbutton_1.clicked.connect(self.set_singleloopplayback)
 
     def inserted(self, start, end):
         self.indexlist.append(len(self.indexlist))
 
+
     def set_ranplayback(self):
         self.setPlaybackMode(QMediaPlaylist.Random)
         random.shuffle(self.indexlist)
-
         # 作用是使当前播放的音乐不变，之后的音乐shuffle过后随机取
         self.curr_index = self.indexlist.index(self.currentIndex())
+        self.parent.playbackbutton_1.show()
+        self.parent.playbackbutton_2.hide()
+        self.parent.playbackbutton_3.hide()
         print(self.indexlist)
 
     def set_seqplayback(self):
         self.setPlaybackMode(QMediaPlaylist.Sequential)
         self.indexlist.sort()
+        self.parent.playbackbutton_2.show()
+        self.parent.playbackbutton_1.hide()
+        self.parent.playbackbutton_3.hide()
 
     def set_singleloopplayback(self):
         self.setPlaybackMode(QMediaPlaylist.CurrentItemInLoop)
+        self.parent.playbackbutton_3.show()
+        self.parent.playbackbutton_1.hide()
+        self.parent.playbackbutton_2.hide()
 
-    # 重载两个函数，主要是为了实现自己的random播放
+    def get_currenturl(self):
+        return self.str_urllist[self.indexlist[self.curr_index]]
+
+    def get_current_whichsong(self):
+        return self.indexlist[self.curr_index]
+
+    # 重载两个函数，主要是为了实现自己的random播放, 这样父类的content的list就会保持不变，随时通过index去取用即可
     def next(self):
         if not self.playbackMode() == QMediaPlaylist.CurrentItemInLoop:
             if self.curr_index == len(self.indexlist) - 1:
@@ -122,6 +151,7 @@ class PlayList(QMediaPlaylist):
             else:
                 self.curr_index += 1
         self.setCurrentIndex(self.indexlist[self.curr_index])
+
 
     def previous(self):
         if not self.playbackMode() == QMediaPlaylist.CurrentItemInLoop:
@@ -138,6 +168,8 @@ class PlayList(QMediaPlaylist):
 # lyric 感觉应该写入这个类， 也就是包含一个lyric 的变量
 
 class Player(QMediaPlayer):
+
+    finish2next = pyqtSignal() # ??
     def __init__(self, parent: Window = None):   # 标注类型辅助，不知道标注Window好，还是Qframe
         super(Player, self).__init__()
         self.setObjectName('player')
@@ -153,7 +185,11 @@ class Player(QMediaPlayer):
 
         # song itself
         self.set_media_content(content=QMediaContent(self.playlist.currentMedia()))
-        self.song_duration = eyed3.load('/Users/yuhu/Desktop/Lyricplayer/musics/' + 'song1.mp3').info.time_secs
+        self.song_duration = 0
+        if not self.playlist.mediaCount() == 0:
+            self.set_song_duration()
+            self.parent.progress_slider.setRange(0, self.song_duration)
+        self.wannaplay = False
 
         # lrc thing
         self.lrc = TxtReader(url='/Users/yuhu/Desktop/日语歌曲翻译/歌词翻译txt版/Lemon.txt')
@@ -163,12 +199,14 @@ class Player(QMediaPlayer):
         # connections
         self.setconnects()
 
+
     def list_test(self):
         baseurl = '/Users/yuhu/Desktop/Lyricplayer/musics/'
-        self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(baseurl + 'song1.mp3')))
-        self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(baseurl + 'song2.mp3')))
-        self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(baseurl + 'song3.mp3')))
-        self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(baseurl + 'Lemon.mp3')))
+        self.add_media_and_url(baseurl + 'song1.mp3')
+        self.add_media_and_url(baseurl + 'song2.mp3')
+        self.add_media_and_url(baseurl + 'song3.mp3')
+        self.add_media_and_url(baseurl + 'Lemon.mp3')
+
         self.playlist.setCurrentIndex(0)
 
     def setconnects(self):
@@ -190,11 +228,28 @@ class Player(QMediaPlayer):
         #song position
         self.positionChanged.connect(self.position_changed)
         self.parent.progress_slider.sliderMoved.connect(self.position_set)
-        self.parent.progress_slider.setRange(0, self.song_duration)
 
         #next prev
         self.parent.nextbutton.clicked.connect(self.next_song)
         self.parent.prevbutton.clicked.connect(self.prev_song)
+        self.finish2next.connect(self.next_song)
+
+        #media changed
+        self.mediaChanged.connect(self.song_changed)
+        self.mediaStatusChanged.connect(self.status_changed)
+
+    def set_song_duration(self):
+        self.song_duration = eyed3.load(self.playlist.get_currenturl()).info.time_secs
+
+    def add_media_and_url(self, url: str):
+        if 'http' in url or 'file' in url:
+            tmpurl = QUrl(url)
+        else:
+            tmpurl = QUrl.fromLocalFile(url)
+        content = QMediaContent(tmpurl)
+        # 下面两句话的顺序不可改变，涉及到信号和槽
+        self.playlist.str_urllist.append(url)  # 暂时不传tmpurl， 若涉及网络需要传tmpurl 可以再改
+        self.playlist.addMedia(content)
 
     def set_media_content(self, url=None, content=None):
         if url:
@@ -223,9 +278,12 @@ class Player(QMediaPlayer):
             pass
 
     def state_changed(self, state):
-        print('state is {}'.format(self.state()))
         if state == QMediaPlayer.StoppedState:
+            self.parent.pausebutton.hide()
+            self.parent.playbutton.show()
             self.currentLrc = 0
+
+            #self.next_song() #这个如果加上，本意是为了歌曲截止的时候跳转，但会出现问题
 
         elif state == QMediaPlayer.PlayingState:
             self.parent.playbutton.hide()
@@ -234,6 +292,17 @@ class Player(QMediaPlayer):
         elif state == QMediaPlayer.PausedState:
             self.parent.pausebutton.hide()
             self.parent.playbutton.show()
+
+    def status_changed(self, status):
+        print(status)
+
+        if status == QMediaPlayer.EndOfMedia:
+            # self.next_song() 这句话不能在这里使用，他妈因为这个函数没到完成medialoaded的时候执行不完。
+            self.finish2next.emit()
+
+        elif status == QMediaPlayer.LoadedMedia and self.wannaplay:
+            self.play()
+
 
     def mute_volume(self):
         self.setVolume(0)
@@ -254,14 +323,26 @@ class Player(QMediaPlayer):
                 self.currentLrc = max(0, i - 1)
                 break
 
+    def song_changed(self):
+        self.set_song_duration()
+        self.parent.progress_slider.setRange(0, self.song_duration)
+
+
     def next_song(self):
         self.playlist.next()
         self.set_media_content(content=self.playlist.currentMedia())
+        print('status before play is {}'.format(self.mediaStatus()))
+        self.wannaplay = True
         self.play()
+
+        print('already playing')
+
+
 
     def prev_song(self):
         self.playlist.previous()
         self.set_media_content(content=self.playlist.currentMedia())
+        self.wannaplay = True
         self.play()
 
     def position_changed(self, position):
@@ -280,7 +361,8 @@ class Player(QMediaPlayer):
             self.currentLrc += 1
 
     # lrc and related
-    def lrc_show(self):    #how to show lrc
+    # how to show lrc
+    def lrc_show(self):
         self.parent.lrc_textBrowser.setPlainText('')
         analyzer_no_parenthesis = self.prettifier.delete_parenthesis(
             self.lrc.analyzer_lst[self.currentLrc])
